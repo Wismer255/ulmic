e0d8d818ab66289ae658fa4b9a37f8ac3c8df053
Michael Wismer <michael.wismer@mpq.mpg.de>
2017-11-11 18:26:08 +0100

    Update GPAW

--- ulmic/external/gpaw.py
+++ ulmic/external/gpaw.py
@@ -1,17 +1,33 @@
+""" Python script for processing GPAW data. """
+from __future__ import absolute_import
+import h5py
+import numpy as np
+from ulmic import AtomicUnits
+from ulmic.external import nearest_neighbor_table
 from gpaw import restart
 from gpaw.kpt_descriptor import KPointDescriptor
 from gpaw.response.pair import PairDensity
 from gpaw.wavefunctions.pw import PWDescriptor
-from ase.units import Bohr
+from gpaw.wannier90 import get_overlap
 
 
-def read_gpaw(input_gpw,nb=None):
+def read_gpaw(input_gpw,nb_max=None,spin_factor=2,
+              output_file='out.hdf5',calculate_overlap=True,
+              calculate_momentum=True):
+    """ Read .gpw file and create a hdf5 as output.
+        Partially adapted from gpaw.wannier90.write_overlaps.
 
-    spin_factor = 2
+        input_gpw:      location of .gpw input file
+        nb_max:         maximum band
+        spin_factor:    electrons per band (only default value supported!)
+    """
+
     nn = 1
 
     au = AtomicUnits()
     atoms, calc = restart(input_gpw, txt=None)
+
+    # Generate full Brillouin zone if necessary
     if calc.parameters['symmetry'] != 'off':
         calc.set(symmetry='off')
         calc.get_potential_energy()
@@ -19,136 +35,104 @@ def read_gpaw(input_gpw,nb=None):
     calc.get_potential_energy()
 
     #Optional parameters:
-    case = input_gpw.replace('.gpw', '')
     nb = calc.parameters['convergence']['bands']
-    if 'nb' in options:
-        nb = int(options['nb'])
+    if nb_max is not None:
+        nb = min(nb,nb_max)
 
     nk_size = calc.parameters['kpts']['size']
     nk = calc.wfs.kd.nbzkpts
     kpts = calc.get_bz_k_points()
     wfs = calc.wfs
     nvalence = calc.setups.nvalence//spin_factor
-    lattice = atoms.get_cell()/au.A
-    reciprocal_lattice = (2*np.pi*au.A)*atoms.get_reciprocal_cell()
+    lattice = atoms.get_cell()/au.AA
+    reciprocal_lattice = (2*np.pi*au.AA)*atoms.get_reciprocal_cell()
 
     bands = range(nb)
     direction = np.eye(3, dtype=int)
-
     klist1d = np.zeros((nk, 3))
     klist3d = np.zeros((nk_size), int)
-    S = np.zeros((nk, 3, 2*nn, nb, nb), complex)
     energy = np.zeros((nk, nb))
-    nn_table = np.zeros((nk, 3, 2*nn), int)
-
-    np.save(output_directory+case+".gpaw.size.npy", nk_size)
-    np.save(output_directory+case+".gpaw.valence.npy", nvalence)
-    np.save(output_directory+case+".gpaw.lattice.npy", lattice)
-    np.save(output_directory+case+".gpaw.reciprocal.npy", reciprocal_lattice)
 
     for i in range(nk):
         klist1d[i, :] = calc.wfs.kd.bzk_kc[i]
-    np.save(output_directory+case+".gpaw.klist1d.npy", klist1d)
 
     for i in range(nk):
-        idx, idy, idz = [int(q) for q in np.rint(nk_size*kpts[i, :])]
-        klist3d[idx, idy, idz] = i
-    np.save(output_directory+case+".gpaw.klist3d.npy", klist3d)
+        ix, iy, iz = [int(q) for q in np.rint(nk_size*kpts[i, :])]
+        klist3d[ix, iy, iz] = i
 
-    for ix in range(nk_size[0]):
-        for iy in range(nk_size[1]):
-            for iz in range(nk_size[2]):
-                i = klist3d[ix, iy, iz]
-                for j in range(-nn, nn):
-                    if j < 0:
-                        nn_table[i, 0, j] = klist3d[(ix+j)%nk_size[0], iy, iz]
-                        nn_table[i, 1, j] = klist3d[ix, (iy+j)%nk_size[1], iz]
-                        nn_table[i, 2, j] = klist3d[ix, iy, (iz+j)%nk_size[2]]
-                    if j >= 0:
-                        nn_table[i, 0, j] = klist3d[(ix+j+1)%nk_size[0], iy, iz]
-                        nn_table[i, 1, j] = klist3d[ix, (iy+j+1)%nk_size[1], iz]
-                        nn_table[i, 2, j] = klist3d[ix, iy, (iz+j+1)%nk_size[2]]
-    np.save(output_directory+case+".gpaw.nn_table.npy", nn_table)
+    nn_table = nearest_neighbor_table(klist3d,nn)
 
-    ef = calc.get_fermi_level()
-    for ik in range(nk):
-        energy[ik, :] = (calc.get_eigenvalues(kpt=ik, spin=0) - ef)[:nb]/au.eV
-    np.save(output_directory+case+".gpaw.energy.npy", energy)
+    energy_fermi = calc.get_fermi_level()
+    for i in range(nk):
+        energy[i, :] = (calc.get_eigenvalues(kpt=i, spin=0) - energy_fermi)[:nb]/au.eV
 
-
-    if not flags['--no-overlap']:
+    if calculate_overlap:
+        overlap = np.zeros((nk, 3, 2*nn, nb, nb), complex)
         icell_cv = (2 * np.pi) * np.linalg.inv(calc.wfs.gd.cell_cv).T
-        r_g = calc.wfs.gd.get_grid_point_coordinates()
-        Ng = np.prod(np.shape(r_g)[1:]) * (False + 1)
+        r_grid = calc.wfs.gd.get_grid_point_coordinates()
+        n_grid = np.prod(np.shape(r_grid)[1:]) * (False + 1)
 
-        u_knG = []
+        u_kng = []
         for i in range(nk):
-            u_knG.append(np.array([wfs.get_wave_function_array(n, i, 0) for n in bands]))
+            u_kng.append(np.array([wfs.get_wave_function_array(n, i, 0) for n in bands]))
 
-        dO_aii = []
-        for ia in calc.wfs.kpt_u[0].P_ani.keys():
-            dO_ii = calc.wfs.setups[ia].dO_ii
-            dO_aii.append(dO_ii)
+        d0_aii = []
+        for i in calc.wfs.kpt_u[0].p_ani.keys():
+            d0_ii = calc.wfs.setups[i].d0_ii
+            d0_aii.append(d0_ii)
 
-        P_kani = []
-        for ik in range(nk):
-            P_kani.append(calc.wfs.kpt_u[0 * nk + ik].P_ani)
-
-        def get_overlap(calc, bands, u1_nG, u2_nG, P1_ani, P2_ani, dO_aii, bG_v):
-            M_nn = np.dot(u1_nG.conj(), u2_nG.T) * calc.wfs.gd.dv
-            r_av = calc.atoms.positions / Bohr
-            for ia in range(len(P1_ani.keys())):
-                P1_ni = P1_ani[ia][bands]
-                P2_ni = P2_ani[ia][bands]
-                phase = np.exp(-1.0j * np.dot(bG_v, r_av[ia]))
-                dO_ii = dO_aii[ia]
-                M_nn += P1_ni.conj().dot(dO_ii).dot(P2_ni.T) * phase
-            return M_nn
+        p_kani = []
+        for i in range(nk):
+            p_kani.append(calc.wfs.kpt_u[0 * nk + i].p_ani)
 
         for ik1 in range(nk):
-            u1_nG = u_knG[ik1]
+            u1_ng = u_kng[ik1]
             for i in range(3):
                 for j in range(-nn, nn):
                     ik2 = nn_table[ik1, i, j]
                     if j < 0:
-                        G = (kpts[ik1] + 1.0*j*direction[i, :]/nk_size)-kpts[ik2]
+                        g_vector = (kpts[ik1] + 1.0*j*direction[i, :]/nk_size)-kpts[ik2]
                     if j >= 0:
-                        G = (kpts[ik1] + 1.0*(j+1)*direction[i, :]/nk_size)-kpts[ik2]
-                    bG_c = kpts[ik2] - kpts[ik1] + G
-                    bG_v = np.dot(bG_c, icell_cv)
-                    u2_nG = u_knG[ik2] * np.exp(-1j * np.inner(r_g.T, bG_v).T)
-                    S[ik1, i, j, :, :] = get_overlap(  calc, 
-                                                       bands, 
-                                                       np.reshape(u1_nG, (len(u1_nG), Ng)), 
-                                                       np.reshape(u2_nG, (len(u2_nG), Ng)), 
-                                                       P_kani[ik1], 
-                                                       P_kani[ik2], 
-                                                       dO_aii, 
-                                                       bG_v)[:nb, :nb]
-            np.save(output_directory+case+".gpaw.S.npy", S)
+                        g_vector = (kpts[ik1] + 1.0*(j+1)*direction[i, :]/nk_size)-kpts[ik2]
+                    bg_c= kpts[ik2] - kpts[ik1] + g_vector
+                    bg_v = np.dot(bg_c, icell_cv)
+                    u2_ng = u_kng[ik2] * np.exp(-1j * np.inner(r_grid.T, bg_v).T)
+                    overlap[ik1, i, j, :, :] = get_overlap(calc,
+                                                           bands,
+                                                           np.reshape(u1_ng, (len(u1_ng), n_grid)),
+                                                           np.reshape(u2_ng, (len(u2_ng), n_grid)),
+                                                           p_kani[ik1],
+                                                           p_kani[ik2],
+                                                           d0_aii,
+                                                           bg_v)[:nb, :nb]
 
-    pair = PairDensity(calc=calc)
-    momentum = np.zeros((nk, nb, nb, 3), complex)
-    for i in range(nk):
-        q_c = [0.0, 0.0, 0.0]
-        qd = KPointDescriptor([q_c])
-        pd = PWDescriptor(pair.ecut, calc.wfs.gd, complex, qd)
-        kptpair = pair.get_kpoint_pair(pd, s=0, K=i, n1=0, n2=nb, m1=0, m2=nb)
-        ol = np.allclose(q_c, 0.0)
-        n_nmvG = pair.get_pair_momentum(pd, kptpair, np.arange(0, nb), np.arange(0, nb))
-        momentum[i, :, :, :] = n_nmvG[..., 0][:nb, :nb, :]
-    np.save(output_directory+case+".gpaw.momentum.npy", momentum)
+    if calculate_momentum:
+        pair = PairDensity(calc=calc)
+        momentum = np.zeros((nk, nb, nb, 3), complex)
+        delta_q_vector = [0.0, 0.0, 0.0]
+        delta_q_descriptor = KPointDescriptor([delta_q_vector])
+        plane_wave_descriptor = PWDescriptor(pair.ecut, calc.wfs.gd, complex,
+                                             delta_q_descriptor)
+        for i in range(nk):
+            kpoint_pair = pair.get_kpoint_pair(plane_wave_descriptor,
+                                               s=0, K=i, n1=0, n2=nb, m1=0, m2=nb)
+            kpoint_momentum = pair.get_pair_momentum(plane_wave_descriptor,
+                                                     kpoint_pair,
+                                                     np.arange(0, nb),
+                                                     np.arange(0, nb))
+            momentum[i, :, :, :] = kpoint_momentum[..., 0]
 
-    hdf5 = h5py.File(output_directory.rstrip('/')+'.hdf5', 'w')
-    dset_energy = hdf5.create_dataset("energy", data=energy)
-    dset_klist1d = hdf5.create_dataset("klist1d", data=klist1d)
-    dset_klist3d = hdf5.create_dataset("klist3d", data=klist3d)
-    dset_lattice = hdf5.create_dataset("lattice", data=lattice)
-    dset_reciprocal = hdf5.create_dataset("reciprocal_lattice",data=reciprocal_lattice)
-    dset_table = hdf5.create_dataset("neighbour_table", data=nn_table)
-    dset_valence = hdf5.create_dataset("valence", data=nvalence)
-    dset_size = hdf5.create_dataset("size", data=nk_size)
-    dset_spin = hdf5.create_dataset("spin_factor", data=spin_factor)
-    dset_momentum = hdf5.create_dataset("momentum", data=momentum)
-    if not flags['--no-overlap']:
-        dset_overlap = hdf5.create_dataset("overlap", data=S)
+    hdf5 = h5py.File(output_file, 'w')
+    hdf5.create_dataset("energy", data=energy)
+    hdf5.create_dataset("klist1d", data=klist1d)
+    hdf5.create_dataset("klist3d", data=klist3d)
+    hdf5.create_dataset("lattice", data=lattice)
+    hdf5.create_dataset("reciprocal_lattice",data=reciprocal_lattice)
+    hdf5.create_dataset("neighbour_table", data=nn_table)
+    hdf5.create_dataset("valence", data=nvalence)
+    hdf5.create_dataset("size", data=nk_size)
+    hdf5.create_dataset("spin_factor", data=spin_factor)
+    if calculate_momentum:
+        hdf5.create_dataset("momentum", data=momentum)
+    if calculate_overlap:
+        hdf5.create_dataset("overlap", data=overlap)
--- ulmic/external/quantum_espresso.py
+++ ulmic/external/quantum_espresso.py
@@ -1,18 +1,27 @@
-"""Convert data from quantum espresso to hdf5. """
+""" Python script for processing Quantum Espresso data. """
 import numpy as np
 import h5py
+from ulmic.external import nearest_neighbor_table
 from ulmic.external.wannier90 import read_mmn
 from ulmic.atomic_units import AtomicUnits
 
 au = AtomicUnits()
 
-def read_quantum_espresso(qe_file,size,case='case',spin_factor=2,overlap_input=None,momentum_input=None,):
-    nn = 1 
+def read_quantum_espresso(qe_file,size,output_file='out.hdf5',spin_factor=2,
+                          overlap_input=None,momentum_input=None,):
+    """
+       qe_file:         quantum espresso output file
+       output_file:     output
+       spin_factor:     spin degeneracy
+       overlap_input:   mmn file
+       momentum_input:  momentum matrix file
+    """
+    nn = 1
     with open(qe_file,'r') as qe_out:
         line = next(qe_out)
 
         while not line.startswith('     lattice parameter (alat)  ='):
-           line = next(qe_out)
+            line = next(qe_out)
         lattice_constant = float(line.split()[-2])
 
         while not line.startswith('     number of electrons       ='):
@@ -58,55 +67,43 @@ def read_quantum_espresso(qe_file,size,case='case',spin_factor=2,overlap_input=N
 
     nk = number_kpoints
     nv = number_valence_bands
-    nb = number_bands
 
     klist3d = np.zeros(tuple(size),int)
     for i in range(nk):
         idx, idy, idz = [int(q) for q in np.rint(size*klist1d[i, :])]
         klist3d[idx, idy, idz] = i
 
-    nn_table = np.zeros((nk,3,2*nn),int)
-    for ix in range(size[0]):
-        for iy in range(size[1]):
-            for iz in range(size[2]):
-                i = klist3d[ix, iy, iz]
-                for j in range(-nn, nn):
-                    if j < 0:
-                        nn_table[i, 0, j] = klist3d[(ix+j)%size[0], iy, iz]
-                        nn_table[i, 1, j] = klist3d[ix, (iy+j)%size[1], iz]
-                        nn_table[i, 2, j] = klist3d[ix, iy, (iz+j)%size[2]]
-                    if j >= 0:
-                        nn_table[i, 0, j] = klist3d[(ix+j+1)%size[0], iy, iz]
-                        nn_table[i, 1, j] = klist3d[ix, (iy+j+1)%size[1], iz]
-                        nn_table[i, 2, j] = klist3d[ix, iy, (iz+j+1)%size[2]]
+    nn_table = nearest_neighbor_table(klist3d,nn)
 
     vol = (np.dot(lv1,np.cross(lv2,lv3)))
-    rv1,rv2,rv3 = 2*np.pi*np.cross(lv2,lv3)/vol,2*np.pi*np.cross(lv3,lv1)/vol,2*np.pi*np.cross(lv1,lv2)/vol
+    rv1,rv2,rv3 = (2*np.pi*np.cross(lv2,lv3)/vol,
+                   2*np.pi*np.cross(lv3,lv1)/vol,
+                   2*np.pi*np.cross(lv1,lv2)/vol)
 
     lattice = np.array([lv1,lv2,lv3])
     reciprocal_lattice = np.array([rv1,rv2,rv3])
- 
+
     if not overlap_input is None:
-        overlap = read_mmn(overlap_input,size,klist1d,klist3d,transpose=False)        
+        overlap = read_mmn(overlap_input,size,klist1d,klist3d,transpose=False)
 
     if not momentum_input is None:
         momentum = np.load(momentum_input)
-        momentum = np.rollaxis(momentum,1,4) 
+        momentum = np.rollaxis(momentum,1,4)
 
-    hdf5 = h5py.File(case+'_quantum_espresso.hdf5', 'w')
-    dset_energy = hdf5.create_dataset("energy", data=energy/au.eV)
-    dset_klist1d = hdf5.create_dataset("klist1d", data=klist1d)
-    dset_klist3d = hdf5.create_dataset("klist3d", data=klist3d)
-    dset_lattice = hdf5.create_dataset("lattice", data=lattice)
-    dset_reciprocal = hdf5.create_dataset("reciprocal_lattice",data=reciprocal_lattice)
-    dset_table = hdf5.create_dataset("neighbour_table", data=nn_table)
-    dset_valence = hdf5.create_dataset("valence", data=nv)
-    dset_size = hdf5.create_dataset("size", data=size)
-    dset_spin = hdf5.create_dataset("spin_factor", data=spin_factor)
+    hdf5 = h5py.File(output_file, 'w')
+    hdf5.create_dataset("energy", data=energy/au.eV)
+    hdf5.create_dataset("klist1d", data=klist1d)
+    hdf5.create_dataset("klist3d", data=klist3d)
+    hdf5.create_dataset("lattice", data=lattice)
+    hdf5.create_dataset("reciprocal_lattice",data=reciprocal_lattice)
+    hdf5.create_dataset("neighbour_table", data=nn_table)
+    hdf5.create_dataset("valence", data=nv)
+    hdf5.create_dataset("size", data=size)
+    hdf5.create_dataset("spin_factor", data=spin_factor)
 
     if not overlap_input is None:
-        dset_overlap = hdf5.create_dataset("overlap", data=overlap)
+        hdf5.create_dataset("overlap", data=overlap)
 
     if not momentum_input is None:
-        dset_momentum = hdf5.create_dataset("momentum", data=momentum)
+        hdf5.create_dataset("momentum", data=momentum)
 
